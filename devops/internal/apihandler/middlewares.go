d0ac98c11881e27e4bdfbc25c9c56b3004bd5bf7
mrh997 <maronghong@bytedance.com>
2025-10-17 19:47:05 +0800

    feat(model_ark): support call option WithReasoningEffort

--- devops/internal/apihandler/middlewares.go
+++ devops/internal/apihandler/middlewares.go
@@ -43,7 +43,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,X-CSRF-Token, Authorization")
-		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-next, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// 放行所有OPTIONS方法
