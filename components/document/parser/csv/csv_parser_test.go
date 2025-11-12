c30a72adfc0a6d7c9262ab06c5969f27082a7667
haoqi <curiousxeon@outlook.com>
2025-11-12 17:41:07 +0800

    feat(parser): 新增document的csv文件parser接口实现

---
+++ components/document/parser/csv/csv_parser_test.go
@@ -0,0 +1,38 @@
+package csv
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cloudwego/eino/components/document/parser"
+	"golang.org/x/net/context"
+)
+
+func TestCsvParser(t *testing.T) {
+	f, err := os.Open("./test.csv")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer f.Close()
+
+	ctx := context.Background()
+	cp, err := NewCsvParser(ctx, &Config{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	docs, err := cp.Parse(ctx, f, parser.WithURI("local"), parser.WithExtraMeta(map[string]any{
+		"_extension": ".csv",
+		"_file_name": "test.csv",
+		"_source":    "local",
+	}))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(docs)
+	return
+}
