8947a5794a9758a852be59365fc5dfd2d3341404
Kaijie Chen <chenkaijie.jay@bytedance.com>
2025-12-16 20:24:57 +0800

    add comments for Filters in ImplOptions

--- components/retriever/opensearch3/option.go
+++ components/retriever/opensearch3/option.go
@@ -23,11 +23,25 @@ import (
 // ImplOptions contains OpenSearch-specific options.
 // Use retriever.GetImplSpecificOptions[ImplOptions] to get ImplOptions from options.
 type ImplOptions struct {
+	// Filters is a list of OpenSearch filter queries.
+	//
+	// Each element in this slice will be marshaled into JSON and added to the
+	// "filter" array of the OpenSearch boolean query.
+	//
+	// You can pass:
+	// 1. map[string]any: e.g., map[string]any{"term": map[string]any{"field": "value"}}
+	// 2. Custom structs: Any Go struct that serializes to a valid OpenSearch Query DSL JSON object.
+	//
+	// This flexibility allows support for the full range of OpenSearch query types
+	// without being limited by fixed Go types.
 	Filters []any `json:"filters,omitempty"`
 }
 
 // WithFilters sets filters for the retrieve query.
 // This may take effect in search modes.
+//
+// filters should be a list of items that can be marshaled into OpenSearch Query DSL (e.g. map[string]any or custom structs).
+// These items are passed directly to the generic OpenSearch SDK and serialized to JSON.
 func WithFilters(filters []any) retriever.Option {
 	return retriever.WrapImplSpecificOptFn(func(o *ImplOptions) {
 		o.Filters = filters
